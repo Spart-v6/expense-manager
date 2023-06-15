3da0bc7caee0b2ed53a047b95ca690fe003c732e
Spart-v6 <ankurnfssingh24@gmail.com>
2023-06-15 21:21:17 +0530

    Modified notification time

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -133,8 +133,8 @@ const SettingsScreen = ({ navigation }) => {
 
   async function scheduleDailyNotifications() {
     const trigger = {
-      hour: 21,
-      minute: 15,
+      hour: 22,
+      minute: 30,
       repeats: true,
     };
 
